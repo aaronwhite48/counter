97331bcdf90a751a8fe98898bac2c31b0f333904
Aaron White <aarondwhite2011@gmail.com>
2020-12-27 22:27:06 -0600

    Counter app

---
+++ src/components/Counter.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+export class Counter extends Component {
+    state = {
+        totalCount: 0
+    }
+
+    // Add one to total
+    addOne = () => {
+        this.setState({ totalCount: this.state.totalCount + 1 });
+    }
+
+    // Subtract one from total
+    subtractOne = () => {
+        this.setState({ totalCount: this.state.totalCount - 1 });
+    }
+
+    // Reset total to zero
+    clearCounter = () => {
+        this.setState({ totalCount: this.state.totalCount * 0 })
+    }
+    
+    render() {
+        return (
+            <div id='counter'>
+                <h2>{ this.state.totalCount }</h2>
+                <div className='counters'>
+                    <button className='updown' onClick={this.subtractOne}>-</button>
+                    <button className='updown' onClick={this.addOne}>+</button>
+                </div>
+
+                <div className='clearCounter'>
+                    <button className='clearbutton' onClick={this.clearCounter}>CLEAR</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Counter
